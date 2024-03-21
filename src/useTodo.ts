1cff20581aaf9d96458179fff0c33c4f815f3438
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-03-21 19:07:53 +0300

    feat(todo): implement add, remove, and toggle todo in a hook

---
+++ src/useTodo.ts
@@ -0,0 +1,27 @@
+import { useCallback, useReducer } from "react";
+import { todoReducer } from './todoReducer';
+
+const useTodo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
+  const add = useCallback((text: string) => {
+    dispatch({ type: "ADD", text });
+  }, []);
+
+  const remove = useCallback((id: number) => {
+    dispatch({ type: "REMOVE", id });
+  }, []);
+
+  const toggle = useCallback((id: number) => {
+    dispatch({ type: "TOGGLE", id });
+  }, []);
+
+  return {
+    add,
+    remove,
+    toggle,
+    todos,
+  };
+};
+
+export default useTodo;
